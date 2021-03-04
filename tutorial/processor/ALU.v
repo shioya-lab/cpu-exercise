b8af8db4e2f517672e84a3c35e21f674bde925b6
toka <tokazerkje@outlook.com>
2021-03-05 04:50:54 +0900

    define types, add alu.v

---
+++ tutorial/processor/ALU.v
@@ -0,0 +1,25 @@
+`include "Types.v"
+
+module ALU(
+    output `DataPath aluOut,
+
+    input `DataPath aluInA,
+    input `DataPath aluInB,
+    input `ALUCodePath code
+);
+
+    always_comb begin
+        case (code)
+
+        `ALU_CODE_ADD:
+            aluout = aluInA + aluInB
+        `ALU_CODE_SUB:
+            aluout = aluInA - aluInB
+        `ALU_CODE_AND:
+            aluout = aluInA & aluInB
+        `ALU_CODE_OR:
+            aluout = aluInA | aluInB
+        `ALU_CODE_SLT:
+            aluout = aluInA < aluInB ? `TRUE : `FALSE;
+
+end module
--- tutorial/processor/Types.v
+++ tutorial/processor/Types.v
@@ -2,3 +2,53 @@
 `include "BasicTypes.v"
 
 // ここより下に，各人の定義を追加してください
+`define OP_WIDTH 6
+`define OP_POS 26
+`define OpPath logic[ `OP_WIDTH-1 : 0 ]
+
+`define FUNCT_WIDTH 6
+`define FUNCT_POS 0
+`define FunctPath logic [ `FUNCT_WIDTH-1 : 0 ]
+
+`define SHAMT_WIDTH 5
+`define SHAMT_POS 5
+`define ShamtPath logic [ `SHAMT_POS +: `SHAMT_WIDTH ]
+
+`define CONSTANT_WIDTH 16
+`define CONSTANT_POS 0
+`define ConstantPath logic [ `CONSTANT_WIDTH-1 : 0 ]
+
+`define RS_POS 21
+`define RD_POS 16
+`define RD_POS 11
+
+`define OP_CODE_ALU 0
+`define OP_CODE_LD 35
+`define OP_CODE_ST 43
+
+`define OP_CODE_ADDI 8
+`define OP_CODE_BEQ 4
+`define OP_CODE_BNE 5
+
+`define FUNCT_CODE_ADD 32
+`define FUNCT_CODE_SUB 34
+`define FUNCT_CODE_AND 36
+`define FUNCT_CODE_OR 37
+`define FUNCT_CODE_SLT 42
+
+`define ALU_CODE_WIDTH 3
+`define ALUCodePath logic [ `ALU_CODE_WIDTH-1: 0 ]
+
+`define ALU_CODE_ADD 0
+`define ALU_CODE_SUB 1
+`define ALU_CODE_AND 2
+`define ALU_CODE_OR 3
+`define ALU_CODE_SLT 4
+
+`define BR_CODE_WIDTH 2
+`define BrCodePath logic [ `BR_CODE_WIDTH-1 : 0 ]
+
+`define BR_CODE_UNTAKEN 0
+`define BR_CODE_TAKEN 1
+`define BR_CODE_EQ 2
+`define BR_CODE_NE 3
